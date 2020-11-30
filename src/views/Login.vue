001bac9bf7a40e7c26172385a3c4de2c2f47432e
markocunj <72965004+markocunj@users.noreply.github.com>
2020-11-30 15:25:38 +0100

    Commit -login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -12,7 +12,7 @@
 id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter
 email" />
  <small id="emailHelp" class="form-text text-muted" >We'll
-never share your email with anyone else.</small>
+never share your e-mail with anyone else.</small>
  </div>
  <div class="form-group">
  <label for="exampleInputPassword1">Password</label>
