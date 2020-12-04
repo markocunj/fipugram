341b035b60d03b481d30578154afb2694dda2017
markocunj <marko.cunj1@gmail.com>
2020-12-04 22:02:26 +0100

    Update Home pagea pomocu v-for i propsa.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,9 +2,11 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-7">
-      <instagram-card />
+      <instagram-card v-for="card in cards" :key="card" :info="card" />
     </div>
-    <div class="col-3">... ovdje ide desni stupac!</div>
+    <div class="col-3">
+      <p v-for="card in cards" :key="card">Ovo je tekst... {{ card }}</p>
+    </div>
   </div>
 </template>
 
@@ -12,8 +14,22 @@
 // @ is an alias to /src
 import instagramCard from "@/components/instagramCard.vue";
 
+let cards = [];
+
+cards = [
+  "https://picsum.photos/id/1/400/400",
+  "https://picsum.photos/id/2/400/400",
+  "https://picsum.photos/id/3/400/400",
+  "https://picsum.photos/id/4/400/400",
+];
+
 export default {
   name: "Home",
+  data: function () {
+    return {
+      cards: cards,
+    };
+  },
   components: {
     instagramCard,
   },
