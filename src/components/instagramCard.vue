1d07e8ac7e3156770a33a68f2e5647384c28c445
markocunj <marko.cunj1@gmail.com>
2020-12-04 22:01:55 +0100

    Update InstagramCard komponente sa v-for petljom.

--- src/components/instagramCard.vue
+++ src/components/instagramCard.vue
@@ -1,16 +1,24 @@
 <template>
-        <div class="card">
-          <div class="card-header">
-            Featured
-           </div>
-          <div class="card-body p-0" >
-            <img class="card-img-top" src="https://picsum.photos/800" />
-          </div>
-        </div>
+  <div class="card">
+    <div class="card-header">Featured</div>
+    <div class="card-body p-0">
+      <img class="card-img-top" :src="info" />
+    </div>
+    <div class="card-footer text-muted">
+      2 days ago
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'instagramCard',
-}
+  props: ["info"],
+  name: "instagramCard",
+};
 </script>
+
+<style lang="scss">
+.card {
+  margin-bottom: 20px;
+}
+</style>
