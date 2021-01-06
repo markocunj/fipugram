542f1fc5b878dda0c1fd832a077b1a4312d8b065
markocunj <marko.cunj1@gmail.com>
2021-01-06 12:22:15 +0100

    Unos podataka u Firebas

--- src/firebase.js
+++ src/firebase.js
@@ -13,4 +13,6 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-export { firebase };
+let db = firebase.firestore();
+
+export { firebase, db };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,29 @@
   <div class="row">
     <div class="col-2"></div>
     <div class="col-7">
+      <form @submit.prevent="postNewImage" class="form-inline mb-5">
+        <div class="form-group">
+          <label for="imageUrl">Image URL</label>
+          <input
+            v-model="newImageUrl"
+            type="text"
+            class="form-control ml-2"
+            placeholder="Enter the image URL"
+            id="imageUrl"
+          />
+        </div>
+        <div class="form-group">
+          <label for="imageDescription">Description</label>
+          <input
+            v-model="newImageDescription"
+            type="text"
+            class="form-control ml-2"
+            placeholder="Enter the image description"
+            id="imageDescription"
+          />
+        </div>
+        <button type="submit" class="btn btn-primary ml-2">Post image</button>
+      </form>
       <instagram-card
         v-for="card in filteredCards"
         :key="card.url"
@@ -34,6 +57,7 @@
 // @ is an alias to /src
 import instagramCard from "@/components/instagramCard.vue";
 import store from "@/store.js";
+import { db } from "@/firebase";
 
 let cards = [];
 
@@ -63,7 +87,31 @@ export default {
       store,
       ime: "",
       prezime: "",
+      newImageDescription: "",
+      newImageUrl: "",
     };
+  },
+  methods: {
+    postNewImage() {
+      const imageUrl = this.newImageUrl;
+      const imageDescription = this.newImageDescription;
+
+      db.collection("posts")
+        .add({
+          url: imageUrl,
+          desc: imageDescription,
+          email: store.currentUser,
+          posted_at: Date.now(),
+        })
+        .then((doc) => {
+          console.log("Spremljeno", doc);
+          this.newImageDescription = "";
+          this.newImageUrl = "";
+        })
+        .catch((e) => {
+          console.error(e);
+        });
+    },
   },
   computed: {
     imePrezime() {
